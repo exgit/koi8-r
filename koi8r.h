0b1f0683021b2585fd506ab08f0777a1a4f5c2ec
oleg.alexeev <oleg.alexeev@inbox.ru>
2019-10-28 21:09:30 +0300

    Minor changes.

--- koi8r.h
+++ koi8r.h
@@ -7,3 +7,4 @@
 size_t koi8r_to_utf8(const char *str, char *buf, size_t size);
 size_t koi8r_to_866(const char *str, char *buf, size_t size);
 size_t koi8r_to_1251(const char *str, char *buf, size_t size);
+
